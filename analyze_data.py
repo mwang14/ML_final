c1b3b9f0d0f6b3e4ab165f27f769ae25bbf14555
Michael Wang <wang.mich@husky.neu.edu>
2018-12-01 15:22:20 -0500

    added preprocessing stuff

--- analyze_data.py
+++ analyze_data.py
@@ -28,12 +28,101 @@ def categories_to_numbers(values):
 df_train = pd.read_csv('UNSW_NB15_training-set.csv')
 df_test = pd.read_csv('UNSW_NB15_testing-set.csv')
 
+def getMeanAndMedian(df):
+  result = {}
+  for column in df:
+    mean_and_median = {}
+    mean=df[column].mean() 
+    median=df[column].median()
+    mean_and_median['mean'] = mean
+    mean_and_median['median'] = median
+    result[column] = mean_and_median
+  return result
 
+#print df_train.dtypes
+def get_correlations(df):
+  df.corr().to_csv('correlations.csv')
+
+def correlation_matrix(df):
+    from matplotlib import cm as cm
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+    cmap = cm.get_cmap('jet', 100)
+    cax = ax1.imshow(df.corr(), interpolation="nearest", cmap=cmap)
+    ax1.grid(True)
+    plt.title('Network Intrusion Feature Correlation')
+    # Add colorbar, make sure to specify tick locations to match desired ticklabels
+    fig.colorbar(cax)
+    plt.show()
+
+
+
+def get_best_distribution(data):
+    dist_names = ["norm","exponweib","weibull_max", "weibull_min", "pareto", "genextreme"]
+    dist_results = []
+    params = {}
+    for dist_name in dist_names:
+        dist = getattr(st, dist_name)
+        param = dist.fit(data)
+        params[dist_name] = param
+        # Applying the Kolmogorov-Smirnov test
+        D, p = st.kstest(data, dist_name, args=param)
+        print("p value for "+dist_name+" = "+str(p))
+        dist_results.append((dist_name, p))
+
+    # select the best fitted distribution
+    best_dist, best_p = (max(dist_results, key=lambda item: item[1]))
+    # store the name of the best fit and its p value
+
+    print("Best fitting distribution: "+str(best_dist))
+    print("Best p value: "+ str(best_p))
+    print("Parameters for the best fit: "+ str(params[best_dist]))
+
+    return best_dist, best_p, params[best_dist]
+
+def isNormal():
+  result = {}
+  for column in X:
+    protos = X[column]
+    loc, scale = st.norm.fit(protos)
+    n = st.norm(loc=loc,scale=scale)
+    x = np.arange(1,len(protos.unique()),1)
+    #plt.hist(protos,bins=x)
+    #plt.plot(x,100000*n.pdf(x))
+    alpha = .01
+    D,p = st.normaltest(protos) 
+    result[column] = p
+  print result
+  return result
+    
+def tryPCA():
+  colors = ['navy', 'turquoise', 'darkorange']
+  lw = 2
+  pca = PCA(n_components=3) 
+  pca.fit(X)
+  X_pca = pca.transform(X)
+  fig = plt.figure()
+  ax = Axes3D(fig)
+  ax.scatter(X_pca[:,0],X_pca[:,1],X_pca[:,2])
+  print pca.explained_variance_ratio_
+  plt.show()
+
+def tryLDA():
+  lda = LinearDiscriminantAnalysis(n_components=3)
+  lda.fit(X,y)
+  X_lda = lda.transform(X)
+  fig = plt.figure()
+  ax = Axes3D(fig)
+  colors = [(0,0,0),(1,1,1),(1,0,0),(0,1,0),(0,0,1),(1,1,0),(1,0,1),(0,1,1),(.5,.5,.5),(.75,0,1)]
+  ax.scatter(X_lda[:,0],X_lda[:,1],X_lda[:,2])
+  plt.show()
+
+  
 #replaces the 
 def process_data(df):
   services = df.service.unique()
   service_replacements = categories_to_numbers(services)
-  #df = df.drop('id',axis=1)
   df['service'].replace(service_replacements,inplace=True)
   
   #replace protocols with categories
@@ -63,96 +152,26 @@ def process_data(df):
     df[column] = pd.cut(df[column],100,labels=False)
 
 
-
-
-#get the X and Y's for each set
-#print df_train['id']
-
 process_data(df_train)
 process_data(df_test)
 
+
+
+
 df_train = df_train.drop('id',axis=1)
-#df_train.to_csv('blah.csv')
+df_test = df_test.drop('id',axis=1)
+df_train.to_csv('train_cleaned.csv')
+df_test.to_csv('test_cleaned.csv')
 y = df_train['attack_cat']
 y2 = df_train['label']
 X = df_train.drop(['attack_cat','label'],axis=1)
-X = pd.DataFrame(preprocessing.scale(X))
+
 X_test = df_test.drop(['attack_cat','label'],axis=1)
-X_test = pd.DataFrame(preprocessing.scale(X_test))
+
 y_test = df_test['attack_cat']
 y_test2 = df_test['label']
-#print X.head()
 
-def correlation_matrix():
-  plt.matshow(df_train.corr())
-  plt.show()
+X = pd.DataFrame(preprocessing.scale(X))
+X_test = pd.DataFrame(preprocessing.scale(X_test))
 
-def get_best_distribution(data):
-    dist_names = ["norm","exponweib","weibull_max", "weibull_min", "pareto", "genextreme"]
-    dist_results = []
-    params = {}
-    for dist_name in dist_names:
-        dist = getattr(st, dist_name)
-        param = dist.fit(data)
-        params[dist_name] = param
-        # Applying the Kolmogorov-Smirnov test
-        D, p = st.kstest(data, dist_name, args=param)
-        print("p value for "+dist_name+" = "+str(p))
-        dist_results.append((dist_name, p))
 
-    # select the best fitted distribution
-    best_dist, best_p = (max(dist_results, key=lambda item: item[1]))
-    # store the name of the best fit and its p value
-
-    print("Best fitting distribution: "+str(best_dist))
-    print("Best p value: "+ str(best_p))
-    print("Parameters for the best fit: "+ str(params[best_dist]))
-
-    return best_dist, best_p, params[best_dist]
-
-def isNormal():
-  result = {}
-  for column in X:
-    print "fitting " + column
-    protos = X[column]
-    print protos
-    loc, scale = st.norm.fit(protos)
-    n = st.norm(loc=loc,scale=scale)
-    x = np.arange(1,len(protos.unique()),1)
-    #plt.hist(protos,bins=x)
-    #plt.plot(x,100000*n.pdf(x))
-    alpha = .01
-    D,p = st.normaltest(protos) 
-    result[column] = p
-  print result
-  return result
-    
-def tryPCA():
-  colors = ['navy', 'turquoise', 'darkorange']
-  lw = 2
-  pca = PCA(n_components=3) 
-  pca.fit(X)
-  X_pca = pca.transform(X)
-  fig = plt.figure()
-  ax = Axes3D(fig)
-  ax.scatter(X_pca[:,0],X_pca[:,1],X_pca[:,2])
-  print pca.explained_variance_ratio_
-  #print len(pca.components_[0]) 
-  print pd.DataFrame(pca.components_, index=['PC-1', 'PC-2','PC-2'], columns=X.columns)
-  #plt.show()
-
-def tryLDA():
-  lda = LinearDiscriminantAnalysis(n_components=3)
-  lda.fit(X,y)
-  X_lda = lda.transform(X)
-  fig = plt.figure()
-  ax = Axes3D(fig)
-  colors = [(0,0,0),(1,1,1),(1,0,0),(0,1,0),(0,0,1),(1,1,0),(1,0,1),(0,1,1),(.5,.5,.5),(.75,0,1)]
-  ax.scatter(X_lda[:,0],X_lda[:,1],X_lda[:,2])
-  plt.show()
-
-def fitSVM():
-  C = 0
-  gamma  0
-  cur_max = 0
-  
---
+++ models.py
@@ -0,0 +1,70 @@
+import preprocessing
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import make_scorer, accuracy_score
+from sklearn.grid_search import GridSearchCV
+from sklearn.svm import LinearSVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.cross_validation import KFold
+import pandas as pd
+import numpy as np
+import pprint as pp
+
+def run_kfolds(clf):
+    kf = KFold(891, n_folds=10)
+    outcomes = []
+    fold = 0
+    for train_index, test_index in kf:
+        fold += 1
+        X_train, X_test = X_all.values[train_index], X_all.values[test_index]
+        y_train, y_test = y_all.values[train_index], y_all.values[test_index]
+        clf.fit(X_train, y_train)
+        predictions = clf.predict(X_test)
+        accuracy = accuracy_score(y_test, predictions)
+        outcomes.append(accuracy)
+        print('Fold {0} accuracy: {1}'.format(fold, accuracy))
+    mean_outcome = np.mean(outcomes)
+    print('Mean accuracy: ', mean_outcome)
+
+#df_train = pd.read_csv('UNSW_NB15_training-set.csv')
+#df_test = pd.read_csv('UNSW_NB15_testing-set.csv')
+df_train = pd.read_csv('train_cleaned.csv')
+df_test = pd.read_csv('test_cleaned.csv')
+
+X_all, y_all, y2_all = preprocessing.gen_X_and_y(df_train)
+X_test_all, y_test_all,y2_test_all = preprocessing.gen_X_and_y(df_test)
+
+num_test = .2
+
+X_train, X_test, y_train, y_test = train_test_split(X_all, y2_all, test_size=num_test)
+print('After splitting training and testing sets')
+scores = []
+#, DecisionTreeClassifier(), GaussianNB(),RandomForestClassifier(),LinearSVC()
+clf_list = [LinearSVC(), DecisionTreeClassifier(), GaussianNB(), RandomForestClassifier()]
+
+# parameters = {'n_estimators': [4, 6, 9],
+#               'max_features': ['log2', 'sqrt', 'auto'],
+#               'criterion': ['entropy', 'gini'],
+#               'max_depth': [2, 3, 5, 10],
+#               'min_samples_split': [2, 3, 5],
+#               'min_samples_leaf': [1, 5, 8]
+#               }
+
+
+# run_kfolds(clf)
+acc_scorer = make_scorer(accuracy_score)
+# grid_obj = GridSearchCV(clf, parameters, scoring=acc_scorer)
+# grid_obj = grid_obj.fit(X_train, y_train)
+# clf = grid_obj.best_estimator_
+accuracy = {}
+for clf in clf_list:
+    clf.fit(X_train, y_train)
+# predictions = clf.predict(X_test)
+    y_pred = clf.predict(X_test_all)
+    score = accuracy_score(y2_test_all, y_pred)
+    accuracy[clf] = score
+
+pp.pprint(accuracy)
+# print('accuracy score: ', accuracy_score(y_test_all, y_pred))
+
